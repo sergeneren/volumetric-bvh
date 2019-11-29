df51f7f107081b060b7b9e3a0511aa5b30ff4fb6
Sergen Eren <sergeneren@gmail.com>
2019-11-29 15:39:10 +0300

    removed obsolete includes. Included bvh_builder

--- main.cpp
+++ main.cpp
@@ -1,31 +1,11 @@
 
-#include <GL/glew.h>
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
 
-
-#include <sstream>
-#include <iostream>
-#include <cuda_runtime.h>
-
-
-#ifndef M_PI
-#define M_PI 3.14156265
-#endif
-
-void Timer(int obsolete) {
-	glutPostRedisplay();
-	glutTimerFunc(10, Timer, 0);
-}
-
+#include "bvh_builder.h"
 
 int main(int argc, char** argv) {
 
 
-
+	
 
 
 
